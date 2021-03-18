dfe9fe7f8192e1bbd234b6a65519b55f1944be20
Andrew Bennett <andrew.bennett3@argos.co.uk>
2021-03-18 20:56:32 +0000

    added simple cache functionality to the Repository

--- app/src/main/java/uk/co/avsoftware/blockchainbrowser/service/repo/BlockchainRepository.kt
+++ app/src/main/java/uk/co/avsoftware/blockchainbrowser/service/repo/BlockchainRepository.kt
@@ -4,4 +4,5 @@ import io.reactivex.rxjava3.core.Single
 
 interface BlockchainRepository {
     fun currentHashRateGigaHashes(): Single<Long>
+    fun flushCache(): Unit
 }
--- app/src/main/java/uk/co/avsoftware/blockchainbrowser/service/repo/impl/BlockChainRepositoryImpl.kt
+++ app/src/main/java/uk/co/avsoftware/blockchainbrowser/service/repo/impl/BlockChainRepositoryImpl.kt
@@ -1,6 +1,7 @@
 package uk.co.avsoftware.blockchainbrowser.service.repo.impl
 
 import io.reactivex.rxjava3.core.Single
+import io.reactivex.rxjava3.subjects.BehaviorSubject
 import uk.co.avsoftware.blockchainbrowser.service.repo.BlockchainRepository
 import uk.co.avsoftware.blockchainbrowser.service.repo.api.BlockchainApi
 import java.util.concurrent.TimeUnit
@@ -8,8 +9,20 @@ import javax.inject.Inject
 
 class BlockChainRepositoryImpl @Inject constructor(private val blockchainApi: BlockchainApi ) : BlockchainRepository {
 
+    private val hashCache: BehaviorSubject<Long> = BehaviorSubject.createDefault(EMPTY)
+
     override fun currentHashRateGigaHashes(): Single<Long> = blockchainApi
         .currentHashRateGigaHashes()
         .timeout(2000, TimeUnit.MILLISECONDS)
+        .doOnSuccess(hashCache::onNext)
+        .concatWith { hashCache.filter { it > 0 } }
+        .firstOrError()
 
+    override fun flushCache() {
+        hashCache.onNext(EMPTY)
+    }
+
+    companion object {
+        private const val EMPTY = -1L
+    }
 }
