c76d402663e567ba6b1589b798e86711ccedfc31
Andrew Bennett <andrew.bennett3@argos.co.uk>
2021-03-21 11:07:46 +0000

    updated HomeViewModelTest

--- app/src/test/java/uk/co/avsoftware/blockchainbrowser/ui/home/HomeViewModelTest.kt
+++ app/src/test/java/uk/co/avsoftware/blockchainbrowser/ui/home/HomeViewModelTest.kt
@@ -1,6 +1,7 @@
 package uk.co.avsoftware.blockchainbrowser.ui.home
 
 import androidx.arch.core.executor.testing.InstantTaskExecutorRule
+import androidx.lifecycle.LiveData
 import androidx.test.espresso.matcher.ViewMatchers.assertThat
 import io.reactivex.rxjava3.core.Single
 import org.hamcrest.Matchers
@@ -10,6 +11,7 @@ import org.junit.rules.TestRule
 import org.mockito.Mockito
 import uk.co.avsoftware.blockchainbrowser.service.repo.BlockchainRepository
 import uk.co.avsoftware.blockchainbrowser.util.mock
+import java.math.BigDecimal
 
 class HomeViewModelTest {
 
@@ -24,13 +26,23 @@ class HomeViewModelTest {
         val blockchainRepository: BlockchainRepository = mock()
 
         Mockito.`when`(blockchainRepository.currentHashRateGigaHashes()).thenReturn(Single.just(209080L))
+        Mockito.`when`(blockchainRepository.getBlockCount()).thenReturn(Single.just(1000L))
+        Mockito.`when`(blockchainRepository.getDifficulty()).thenReturn(Single.just(BigDecimal.ONE))
+        Mockito.`when`(blockchainRepository.getLatestHash()).thenReturn(Single.just("000000dsdbfdhavfdvfkf"))
 
+        // When
         val viewModel = HomeViewModel(blockchainRepository)
 
-        viewModel.hashRate.observeForever {
-            assertThat(it, Matchers.`is`(209080L))
-        }
-
+        // Then
+        assertLiveData(viewModel.hashRate, 209080L)
+        assertLiveData(viewModel.blockCount, 1000L)
+        assertLiveData(viewModel.difficulty, BigDecimal.ONE.toString())
+        assertLiveData(viewModel.latestHash, "000000dsdbfdhavfdvfkf")
     }
+
+    private fun <T> assertLiveData( liveData: LiveData<T>, expectedValue: T) =
+        liveData.observeForever {
+            assertThat(it, Matchers.`is`(expectedValue))
+        }
 
 }
