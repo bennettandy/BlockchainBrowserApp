94761727ad9a5651845777bd3158a2f761fceb29
Andrew Bennett <andrew.bennett3@argos.co.uk>
2021-03-22 09:39:57 +0000

    set up data binding for dashboard fragment

--- app/src/main/java/uk/co/avsoftware/blockchainbrowser/ui/dashboard/DashboardFragment.kt
+++ app/src/main/java/uk/co/avsoftware/blockchainbrowser/ui/dashboard/DashboardFragment.kt
@@ -5,14 +5,17 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.TextView
+import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
 import uk.co.avsoftware.blockchainbrowser.R
+import uk.co.avsoftware.blockchainbrowser.databinding.FragmentDashboardBinding
 
 class DashboardFragment : Fragment() {
 
     private lateinit var dashboardViewModel: DashboardViewModel
+    private lateinit var viewBinding: FragmentDashboardBinding
 
     override fun onCreateView(
             inflater: LayoutInflater,
@@ -21,11 +24,11 @@ class DashboardFragment : Fragment() {
     ): View? {
         dashboardViewModel =
                 ViewModelProvider(this).get(DashboardViewModel::class.java)
-        val root = inflater.inflate(R.layout.fragment_dashboard, container, false)
-        val textView: TextView = root.findViewById(R.id.text_dashboard)
-        dashboardViewModel.text.observe(viewLifecycleOwner, Observer {
-            textView.text = it
-        })
-        return root
+
+        viewBinding = DataBindingUtil.inflate(inflater, R.layout.fragment_dashboard, container, false)
+        viewBinding.lifecycleOwner = this
+        viewBinding.viewmodel = dashboardViewModel
+
+        return viewBinding.root
     }
 }
